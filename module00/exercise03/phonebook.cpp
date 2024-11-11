59f8ceb45e086cf3d1d4b8e90642472052638999
Rui Rocha <rui.rocha@ctw.bmwgroup.com>
2024-11-11 01:20:52 +0000

    Added contact index to print functions of phonebook

--- module00/exercise03/phonebook.cpp
+++ module00/exercise03/phonebook.cpp
@@ -22,7 +22,10 @@ void Phonebook::print() const {
         std::cout << "No contacts\n";
         return;
     }
-    for(const Contact& i : contacts) i.print();
+    for(auto i = 0; i < contacts.size(); ++i) {
+        std::cout << i << " - ";
+        contacts[i].print();
+    };
 }
 
 void Phonebook::print_bookmarks() const {
@@ -34,7 +37,10 @@ void Phonebook::print_bookmarks() const {
         std::cout << "No bookmarks\n";
         return;
     }
-    for(const Contact& i : bookmarks) i.print();
+    for(auto i = 0; i < bookmarks.size(); ++i) {
+        std::cout << i << " - ";
+        bookmarks[i].print();
+    };
 }
 
 std::optional<Contact> Phonebook::contact(int index) {
